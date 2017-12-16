9ad377d2e02bc7e9e8a8b7ebf5bd09ea3ef23ecd
土居 健人 <dk-arts-23pm@doi-no-MacBook-Air.local>
2017-12-16 16:01:03 +0900

    add the weight part

--- focal_loss_multiclass.py
+++ focal_loss_multiclass.py
@@ -16,7 +16,7 @@ class MultiClassForcalLoss(nn.Module):
         if input.dim()>2:
             input = input.view(input.size(0), input.size(1), -1)
             input = input.transpose(1,2)
-            input = onput.contiguous().view(-1, input.size(2)).squeeze()
+            input = input.contiguous().view(-1, input.size(2)).squeeze()
         target = target(-1, 1)
 
         # compute the negative likelyhood
@@ -26,11 +26,18 @@ class MultiClassForcalLoss(nn.Module):
         pt = Variable(logpt.data.exp())
 
         # implement the class balancing (Coming soon...)
-        # if self.weight is not None:
-        #    aaaaaaa
+        if self.weight is not None:
+            if self.weight.type() != input.data.type():
+                self.weight = self.weight.type_as(input.data)
+            b_h_w, c = input.size()
+            weight_tensor = torch.zeros(b_h_w, 1)
+            weight_tensor[:,0] = weight[target.byte().squeeze()]
 
         # compute the loss
-        loss = -1 * (1-pt)**self.gamma * logpt
+        if self.weight is not None:
+            loss = -1 * weight_tensor * (1-pt)**self.gamma * logpt
+        else:
+            loss = -1 * (1-pt)**self.gamma * logpt
 
         # averaging (or not) loss
         if self.size_average:
