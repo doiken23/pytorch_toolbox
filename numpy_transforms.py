7084f51bf27f026772401e8a09ba124f24b26d58
doiken23 <doi-kento-astro@g.ecc.u-tokyo.ac.jp>
2018-02-17 21:33:02 +0900

    fix the small bug

--- numpy_transforms.py
+++ numpy_transforms.py
@@ -8,23 +8,26 @@ class Numpy_Flip(object):
     def __init__(self):
         pass
 
-    def __call__(self, image_array, p=0.5):
+    def __call__(self, data_list, p=0.5):
+        image_array, target = data_list
+
         if random.random() < p:
             image_array = image_array[:,:,::-1]
 
         if random.random() < p:
             image_array = image_array[:,::-1,:]
 
-        return image_array
+        return [image_array, target]
 
 class Numpy_Rotate(object):
 
     def __init__(self):
         pass
 
-    def __call__(self, image_array):
+    def __call__(self, data_list):
+        image_array, target = data_list
+        
         angle = random.choices([0, 1, 2, 3])
-
         image_array = np.rot90(image_array, angle[0], (1,2))
 
-        return image_array
+        return [image_array, target]
