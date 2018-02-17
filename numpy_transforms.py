be434fd7fa7653b591e081f6b4b1a201a8e0defb
doiken23 <doi-kento-astro@g.ecc.u-tokyo.ac.jp>
2018-02-17 17:43:11 +0900

    the first commit

---
+++ numpy_transforms.py
@@ -0,0 +1,30 @@
+import torch
+
+import numpy as np
+import random
+
+class Numpy_Flip(object):
+
+    def __init__(self):
+        pass
+
+    def __call__(self, image_array, p=0.5):
+        if random.random() < p:
+            image_array = image_array[:,:,::-1]
+
+        if random.random() < p:
+            image_array = image_array[:,::-1,:]
+
+        return image_array
+
+class Numpy_Rotate(object):
+
+    def ___init__(sel):f
+        pass
+
+    def __call__(self, image_array):
+        angle = random.choices([0, 1, 2, 3])
+
+        image_array = np.rot90(image_array, angle[0], (1,2))
+
+        return image_array
