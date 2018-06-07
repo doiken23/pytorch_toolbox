affa9ff98c6e4262998ff2a3676b9cff6ce6a8da
Doi Kento <doi-kento-astro@g.ecc.u-tokyo.ac.jp>
2018-06-07 16:48:51 +0900

    first commit

---
+++ utils.py
@@ -0,0 +1,12 @@
+import csv
+
+def writeout_augs(args, out_dir):
+    fout = open(Path(out_dir).joinpath('arguments.csv', "wt"))
+    csvout = csv.writer(fout)
+    print('*' * 20)
+    print('Write out Arguments...')
+    print('*' * 20)
+    for arg in dir(args):
+        if not arg.startswith('_'):
+            csvout.writerow([arg, str(getattr(args, arg))])
+            print('%-25s %-25s' % (arg , str(getattr(args, arg))))
